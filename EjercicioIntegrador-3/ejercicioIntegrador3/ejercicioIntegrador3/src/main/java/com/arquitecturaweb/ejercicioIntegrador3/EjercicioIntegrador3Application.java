d5a2b3f2b9ba0d8c7c89b94f1b91810e60ff1abb
Candela Echazu <gcandela894@gmail.com>
2025-10-13 10:08:57 -0300

    Se crea proyecto con Spring Boot

---
+++ EjercicioIntegrador-3/ejercicioIntegrador3/ejercicioIntegrador3/src/main/java/com/arquitecturaweb/ejercicioIntegrador3/EjercicioIntegrador3Application.java
@@ -0,0 +1,13 @@
+package com.arquitecturaweb.ejercicioIntegrador3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioIntegrador3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioIntegrador3Application.class, args);
+	}
+
+}
