5efdc280bebe02a4e0d57550f895a2cec8f223f6
Joaquin Hevia <joaco.r.hevia@gmail.com>
2025-11-03 17:37:34 -0300

    agrego estructura de proyecto 4

---
+++ EjercicioIntegrador-4/Integrador-4/microservicio-administrador/src/main/java/org/example/microservicioadministrador/MicroservicioAdministradorApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservicioadministrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioAdministradorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioAdministradorApplication.class, args);
+    }
+
+}
