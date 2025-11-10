73d4185045c2c62873fca679fb1dbf7a08dab37a
emilia <memiliatunesi@gmail.com>
2025-11-10 18:49:30 -0300

    subo micro

--- EjercicioIntegrador-4/Integrador-4/microservicio-usuario/src/main/java/com/trabajointegrador/microserviciousuario/repository/CuentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trabajointegrador.microserviciousuario.repository;
-
-import com.trabajointegrador.microserviciousuario.entity.Cuenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
-}
